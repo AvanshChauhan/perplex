31994e99f337b317f3ea4c4e3beedc534aa7c08a
AvanshChauhan <avanshsingh2017@gmail.com>
2026-06-08 12:23:22 +0530

    first commit

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "ai"],
+      required: true,
+    },
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
